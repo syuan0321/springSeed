36568299f8cbbedbccbf886dca03dbab22fdcdb0
syuan0321 <syuan0321@gmail.com>
2017-08-22 11:51:05 -0400

    init commit. Spring Seed

---
+++ src/main/java/com/derek/seed/HelleWorld.java
@@ -0,0 +1,22 @@
+package com.derek.seed;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+
+public class HelleWorld implements BeanPostProcessor{
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("Hellow There~~~~~~~~");
+		
+		
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
